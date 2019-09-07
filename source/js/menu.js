d00322b767dcebe57ca980f140eed157063028af
Alina <lukinaaline@gmail.com>
2019-09-07 14:51:37 +0300

    final3

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,22 +2,29 @@ var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.page-header__toggle');
 var pageHeader = document.querySelector('.page-header');
 
-    navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav--closed');
+navMain.classList.remove('main-nav--opened');
+navToggle.classList.remove('page-header__toggle--opened');
+navToggle.classList.add('page-header__toggle--closed');
+pageHeader.classList.remove('page-header--opened');
+pageHeader.classList.add('page-header--closed');
 
-    navToggle.addEventListener('click', function() {
-if (navMain.classList.contains('main-nav--closed')) {
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
     navToggle.classList.remove('page-header__toggle--closed');
     navToggle.classList.add('page-header__toggle--opened');
     pageHeader.classList.remove('page-header--closed');
     pageHeader.classList.add('page-header--opened');
-} else {
+  } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
     navToggle.classList.remove('page-header__toggle--opened');
     navToggle.classList.add('page-header__toggle--closed');
     pageHeader.classList.remove('page-header--opened');
     pageHeader.classList.add('page-header--closed');
-}
-  });
+  }
+});
